3b2af23d64a396ae1b85efde7f68585e2b51a87d
Ali Azadi <aliazadi8494@gmail.com>
2021-09-18 18:30:22 +0430

    Made the start_translation command for the bot and fixed the start command code

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ USERNAME = ""
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+# Starting the conversation with the user
 def start(update, context):
 
     global USERNAME
@@ -28,3 +29,17 @@ def start(update, context):
     welcome_text = """Hi {}!
 Hope you're doing great!
 In order to translate your first text using this bot,Type /translate."""
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_text)
+
+
+TEXT, LANGUAGE = range(2)
+# Asking the user for the text
+def start_translation(update, context):
+
+    explanation_text = "What's the text you want to translate?!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=explanation_text)
+
+    return TEXT
+
+
+
